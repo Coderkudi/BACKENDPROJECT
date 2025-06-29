01b9fe62d640b1b2d3229f2a38085a18be64ea3d
Rimmi Goyal <goyalrimmi12@gmail.com>
2025-06-29 20:23:27 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,34 @@
+import { request } from "express"
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+ }
+
+
+
+
+
+export { asyncHandler }
+
+
+//example usage 
+// const asyncHandler = () => { }
+// const asyncHandler = (func) => { () => { } }
+// const asyncHandler = (func) => { async () => { } }
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     }
+//     catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+//  }
+
