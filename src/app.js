de020639ea2c1e8de57107b59219078a116ede4d
Rimmi Goyal <goyalrimmi12@gmail.com>
2025-07-01 21:44:12 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -19,11 +19,11 @@ app.use(cookieParser())
 
 //routes import
 
-import router from './routes/user.routes.js'
+import userRouter from './routes/user.routes.js'
 
 
 //routes declaration 
-app.use("/api/v1/users", router)
+app.use("/api/v1/users", userRouter)
 
 // http://localhost:8000/api/v1/users/register
 
